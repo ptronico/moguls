09e6b19d1638d561f2e0a883ec8e98fb5069f4af
Pedro Vasconcelos <ptronico@gmail.com>
2025-10-27 11:22:35 -0300

    feat: improving Participant model

--- src/apps/participants/models.py
+++ src/apps/participants/models.py
@@ -4,14 +4,18 @@ from apps.commons.models import BaseModel
 
 
 class Participant(BaseModel):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-
     class Meta:
         ordering = ["first_name", "last_name"]
         constraints = [
             models.UniqueConstraint(fields=["first_name", "last_name"], name="unique_full_name"),
         ]
 
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
     def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
